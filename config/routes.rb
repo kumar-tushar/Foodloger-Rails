27bfa54dea658f9329f3e27d6cb76dc7cee0e0d3
kumar-tushar <tushar.workplace@gmail.com>
2022-06-23 17:33:16 +0530

    encoding done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :entries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "entries#index"
 end
